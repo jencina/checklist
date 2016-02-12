47f1e6fc51ece618845a2d88017f768a62087229
jencina <jon.encina@uandresbello.edu>
2016-02-12 15:06:22 -0300

    asd
    asd

--- protected/views/checklist/update.php
+++ protected/views/checklist/update.php
@@ -946,7 +946,7 @@
         'booster.widgets.TbButton',
         array(
             'buttonType' => 'submit',
-            'context' => 'primary',
+            'type' => 'primary',
             'label' => 'Guardar'
         )
     ); ?>
